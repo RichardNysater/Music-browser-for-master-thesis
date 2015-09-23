13871f2d478cc49071a6d41f91efc7759108bd5b
RichardNysater <richard@nysater.org>
2015-09-23 18:33:55 +0200

    First version of player-navbar added.

--- app/player/player.js
+++ app/player/player.js
@@ -18,33 +18,21 @@ angular.module('myApp.player', ['ngRoute'])
     $scope.songs = [
       {
         id: 'one',
-        title: 'Rain',
-        artist: 'Drake',
-        url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/rain.mp3'
+        title: 'Test1',
+        artist: 'Testartist1',
+        url: '/app/music/test1.mp3'
       },
       {
         id: 'two',
-        title: 'Walking',
-        artist: 'Nicki Minaj',
-        url: 'http://www.schillmania.com/projects/soundmanager2/demo/_mp3/walking.mp3'
+        title: 'Test2',
+        artist: 'Testartist2',
+        url: '/app/music/test2.wav'
       },
       {
         id: 'three',
-        title: 'Barrlping with Carl (featureblend.com)',
-        artist: 'Akon',
-        url: 'http://www.freshly-ground.com/misc/music/carl-3-barlp.mp3'
-      },
-      {
-        id: 'four',
-        title: 'Angry cow sound?',
-        artist: 'A Cow',
-        url: 'http://www.freshly-ground.com/data/audio/binaural/Mak.mp3'
-      },
-      {
-        id: 'five',
-        title: 'Things that open, close and roll',
-        artist: 'Someone',
-        url: 'http://www.freshly-ground.com/data/audio/binaural/Things%20that%20open,%20close%20and%20roll.mp3'
+        title: 'Test3',
+        artist: 'Testartist3',
+        url: '/app/music/test3.mp3'
       }
     ];
   }]);
