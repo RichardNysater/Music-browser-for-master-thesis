888a8fbcfc7214997fe345df71b61b1e223c96a7
RichardNysater <richard@nysater.org>
2015-09-23 16:43:47 +0200

    Imported musicplayer

---
+++ app/player/player_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.player module', function() {
+
+  beforeEach(module('myApp.player'));
+
+  describe('player controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var PlayerController = $controller('PlayerController');
+      expect(PlayerController).toBeDefined();
+    }));
+
+  });
+});
