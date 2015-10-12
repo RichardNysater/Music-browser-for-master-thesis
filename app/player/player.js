b9c6cded5ef37e6b1db1655f1bfe3c09ef752a43
RichardNysater <richard@nysater.org>
2015-10-12 18:00:59 +0200

    Small code fixes

--- app/player/player.js
+++ app/player/player.js
@@ -12,7 +12,6 @@ angular.module('myApp.player', ['ngRoute'])
 .controller('PlayerController', ['$scope','Api',
   function ($scope, Api) {
     $scope.songs = Api.Music.query(); //Inject songs into scope
-    $scope.leftPlayerIcons = [{"id":"prev","directive":"prev-track"}];
     /**
      * Sets current volume
      * @param volume The volume (0-100) to play at
@@ -20,6 +19,7 @@ angular.module('myApp.player', ['ngRoute'])
     $scope.setvolume = function(volume){
       $scope.volume = volume;
     };
+
 
 
     /**
