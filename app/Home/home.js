2af07f47630f390b59359e6d97271fcc838e085a
RichardNysater <richard@nysater.org>
2015-10-30 17:01:16 +0100

    Added landing page

---
+++ app/Home/home.js
@@ -0,0 +1,13 @@
+angular.module('myApp.home', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'home/home.html',
+            controller: 'HomeController'
+        });
+    }])
+
+    .controller('HomeController', ['$scope',
+        function ($scope) {
+
+        }]);
