495c52c4f8891a6af14fb0fb0a27cf8b29613727
RichardNysater <richard@nysater.org>
2015-11-05 21:47:17 +0100

    Now correctly clears playlist before trying to add more songs.

--- app/js/songRequestService.js
+++ app/js/songRequestService.js
@@ -22,6 +22,22 @@ SongRequestService.service('SongRequestService', ['$resource','$http','angularPl
             return $http(req);
         };
 
+        var addSongs = function(songs){
+            var key = null;
+            for (var i = 0; i < songs.length; i++) {
+                var id = songs[i].songID;
+                var tmp = angularPlayer.addTrack({
+                    "title": id,
+                    "id": id,
+                    "url": "/app/api/music/" + id + ".ogg"
+                });
+                if (key === null) {
+                    key = tmp;
+                }
+
+            }
+            angularPlayer.playTrack(key);
+        };
         /**
          * Sends a request to the server for all songs matching the input features
          * and adds them to the playlist.
@@ -29,15 +45,21 @@ SongRequestService.service('SongRequestService', ['$resource','$http','angularPl
          * @param featurelist The features to match
          */
         this.playMatchingSongs = function(featurelist){
+
             var request = this.sendRequest(featurelist);
 
             request.then(function successCallback(response) {
                 var res = response.data;
-                for(var i = 0; i<res.length;i++){
-                    var id =res[i].songID;
-                    angularPlayer.addTrack({"title":id,"id":id,"url":"/app/api/music/"+id+".ogg"});
+                if(res.length > 0) {
+                    if (angularPlayer.getPlaylist().length > 0) {
+                        angularPlayer.clearPlaylist(function(param){
+                            addSongs(res);
+                        });
+                    }
+                    else {
+                        addSongs(res);
+                    }
                 }
-                angularPlayer.play();
             }, function errorCallback() {
                 console.log("Failed to query for songs.");
             });
