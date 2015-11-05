a80f706092ff6aa0e0fd0ef16bb040ace6c3b311
RichardNysater <richard@nysater.org>
2015-11-05 21:46:04 +0100

    Changed how the plane side works. Now selects a feature on each axis and finds songs based on where you click in the plane.

--- app/plane/plane.js
+++ app/plane/plane.js
@@ -9,14 +9,14 @@ angular.module('myApp.plane', ['ngRoute'])
   });
 }])
 
-.controller('PlaneCtrl', ['$scope','Api',
-      function ($scope,Api) {
+.controller('PlaneCtrl', ['$scope','Api','SongRequestService',
+      function ($scope,Api,SongRequestService) {
 
         //Get labels and initialize select boxes
-        Api.Labels.query().$promise.then(function(data){
+        Api.Features.query().$promise.then(function(data){
           $scope.labels = data;
           $scope.firstSelect = data[0];
-          $scope.secondSelect = data[1];
+          $scope.secondSelect = data[data.length-1];
         }, function(err){
           throw "No labels were returned by query: "+err;
         });
@@ -26,17 +26,24 @@ angular.module('myApp.plane', ['ngRoute'])
          * @param event Event
          */
           $scope.planeClick = function(event) {
+            var FEATURE_VARIANCE = 10;
 
             $scope.xc = event.offsetX;
             $scope.yc = event.offsetY;
 
-            $scope.imgwidth = 10;
-            $scope.imgheight = 10;
 
+            $scope.imgwidth = $('.plane').outerWidth()*(FEATURE_VARIANCE/100);
+            $scope.imgheight = $('.plane').outerHeight()*(FEATURE_VARIANCE/100);
+            console.log($scope.imgheight);
             $scope.imgleft = event.pageX-($scope.imgwidth/2);
             $scope.imgtop = event.pageY-($scope.imgheight/2);
             $scope.xpercent = Math.round(100*($scope.xc/$('.plane').outerWidth()));
             $scope.ypercent = Math.round(100*($scope.yc/($('.plane').outerHeight())));
+
+
+            var xFeature = {feature:{id:$scope.firstSelect.id},minvalue:$scope.xpercent-FEATURE_VARIANCE,maxvalue:$scope.xpercent+FEATURE_VARIANCE};
+            var yFeature = {feature:{id:$scope.secondSelect.id},minvalue:$scope.ypercent-FEATURE_VARIANCE,maxvalue:$scope.ypercent+FEATURE_VARIANCE};
+            SongRequestService.playMatchingSongs([xFeature,yFeature]);
         }
 
 
