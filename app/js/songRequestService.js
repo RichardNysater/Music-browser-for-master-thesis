4cb84faffa4304118101cd063d7c0f42afabd610
RichardNysater <richard@nysater.org>
2015-11-05 19:46:04 +0100

    Added ability to add matching songs to playlist.

--- app/js/songRequestService.js
+++ app/js/songRequestService.js
@@ -3,8 +3,8 @@ var SongRequestService = angular.module('SongRequestService', ['ngResource']);
 /**
  * Handle the sending of requests for songs to the server
  */
-SongRequestService.service('SongRequestService', ['$resource','$http',
-    function($resource,$http) {
+SongRequestService.service('SongRequestService', ['$resource','$http','angularPlayer',
+    function($resource,$http,angularPlayer) {
 
         /**
          * Sends a request to the server for songs matching the input features
@@ -19,10 +19,29 @@ SongRequestService.service('SongRequestService', ['$resource','$http',
                     features: featurelist
                 }
             };
-
             return $http(req);
-        }
+        };
 
+        /**
+         * Sends a request to the server for all songs matching the input features
+         * and adds them to the playlist.
+         *
+         * @param featurelist The features to match
+         */
+        this.playMatchingSongs = function(featurelist){
+            var request = this.sendRequest(featurelist);
+
+            request.then(function successCallback(response) {
+                var res = response.data;
+                for(var i = 0; i<res.length;i++){
+                    var id =res[i].songID;
+                    angularPlayer.addTrack({"title":id,"id":id,"url":"/app/api/music/"+id+".ogg"});
+                }
+                angularPlayer.play();
+            }, function errorCallback() {
+                console.log("Failed to query for songs.");
+            });
+        }
     }
 ]);
 
