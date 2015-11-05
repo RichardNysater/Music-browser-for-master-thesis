28d67b99ab80f9fed211fcb5e65c13692c6db81f
RichardNysater <richard@nysater.org>
2015-11-05 21:45:09 +0100

    Added ability to search for songs using the sliders.

--- app/sliders/sliders.js
+++ app/sliders/sliders.js
@@ -9,21 +9,31 @@ angular.module('myApp.sliders', ['ngRoute'])
   });
 }])
 
-.controller('SlidersCtrl', ['$scope','Api','SongRequestService',
-      function ($scope, Api, SongRequestService) {
+.controller('SlidersCtrl', ['$scope','Api','SongRequestService','angularPlayer',
+      function ($scope, Api, SongRequestService,angularPlayer) {
           $scope.featurelist = [];
 
           /**
            * Build the featurelist with all features
            */
           Api.Features.query().$promise.then(function(data){
-
              for(var i = 0; i<data.length; i++){
                  $scope.featurelist.push({"feature":data[i], "minvalue":0,"maxvalue":100});
               }
           }, function(err){
               throw "No features were returned by query: "+err;
           });
+
+          $scope.sendRequest = function(){
+              /*
+              var req = SongRequestService.sendRequest($scope.featurelist);
+              req.then(function successCallback(response) {
+                  console.log(response.body);
+              }, function errorCallback(response) {
+                  console.log("fail");
+              });*/
+              SongRequestService.playMatchingSongs($scope.featurelist);
+          }
 
           /**
            * Adds a percentage sign after the number on the slider
