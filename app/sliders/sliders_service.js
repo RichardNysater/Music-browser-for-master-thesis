1efc4dd2f15fdffc75c7742f3744dec63df695cc
RichardNysater <richard@nysater.org>
2015-11-11 16:34:54 +0100

    Added a new service for the sliders.

---
+++ app/sliders/sliders_service.js
@@ -0,0 +1,18 @@
+var SlidersService = angular.module('SlidersService',[]);
+
+/**
+ * Serves as storage for values selected from the sliders.
+ */
+SlidersService.factory('SlidersService', function() {
+    var SlidersService = {};
+
+    /**
+     * Should be called to save values selected in the sliders
+     * @param features The features to save
+     */
+    SlidersService.saveSliders = function (features) {
+        SlidersService.features = features;
+    };
+
+    return SlidersService;
+});
