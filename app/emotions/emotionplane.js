c3e520ceecf4171f4547273b0f440696227f7614
RichardNysater <richard@nysater.org>
2015-12-10 14:39:25 +0100

    Changed the underlying algorithm for the emotion plane.

--- app/emotions/emotionplane.js
+++ app/emotions/emotionplane.js
@@ -18,67 +18,40 @@ angular.module('myApp.emotions', ['ngRoute'])
         function ($scope,Api,SongRequestService,EmotionService) {
 
             /**
-             * Calculate how important each emotion is to a coordinate.
-             * @param xcoord The x-coordinate inside the plane.
-             * @param ycoord The y-coordinate inside the plane.
+             * Calculate the min-max values of each feature based on the distance between the click and the corners
+             * @param x_coord The x-coordinate inside the plane.
+             * @param y_coord The y-coordinate inside the plane.
              * @param plane_width The total width of the plane
              * @param plane_height The total height of the plane
-             * @returns {{}} A mapping between the emotions and their importance
+             * @returns {Array} An array with each feature's min and max value calculated
              */
-            var calcImportance = function(xcoord, ycoord, plane_width, plane_height){
-
-                /* Calculate the distances */
-                var distanceTL = Math.sqrt(Math.pow(xcoord - 0, 2) + Math.pow(ycoord - 0, 2)); // Distance to top left corner
-                var distanceTR = Math.sqrt(Math.pow(xcoord - plane_width, 2) + Math.pow(ycoord - 0, 2)); // Distance to top right corner
-                var distanceBL = Math.sqrt(Math.pow(xcoord - 0, 2) + Math.pow(ycoord - plane_height, 2)); // Distance to bottom left corner
-                var distanceBR = Math.sqrt(Math.pow(xcoord - plane_width, 2) + Math.pow(ycoord - plane_height, 2)); // Distance to bottom right corner
-
-                /* Calculate  how close to a corner the position is from 1 to 0 */
-                var max_nearby_distance = (plane_height+plane_width)/2;
-                var TL = 1-Math.min(1,distanceTL/(max_nearby_distance));
-                var TR = 1-Math.min(1,distanceTR/(max_nearby_distance));
-                var BL = 1-Math.min(1,distanceBL/(max_nearby_distance));
-                var BR = 1-Math.min(1,distanceBR/(max_nearby_distance));
-
-                /* Calculate the importance of each corner */
-                var sum = TL+TR+BL+BR;
-                TL = TL*4/sum;
-                TR = TR*4/sum;
-                BL = BL*4/sum;
-                BR = BR*4/sum;
-
-                TL = TL.toFixed(2);
-                TR = TR.toFixed(2);
-                BL = BL.toFixed(2);
-                BR = BR.toFixed(2);
-
-                var map = {};
-                map[$scope.topleft] = TL;
-                map[$scope.topright] = TR;
-                map[$scope.bottomleft] = BL;
-                map[$scope.bottomright] = BR;
-                return map;
-            };
-
-            /**
-             * Calculate the min-max values of each feature based on the importance of each emotion.
-             * @param emotion_importance The importance of the four emotions
-             */
-            var calcFeatures = function(emotion_importance){
+            var calcFeatures = function(x_coord, y_coord, plane_width, plane_height){
                 var feature_list = [];
 
-                for(var i = 0; i < $scope.emotions[0].features.length;i++){
-                    var maxValSum = 0, minValSum = 0;
+                /* Scale the coordinate system to be between -1 and 1 */
+                var x =2*((x_coord/plane_width)-0.5);
+                var y = 2*(0.5-(y_coord/plane_height));
 
-                    /* Each emotion should have a set of features with a min-value and max-value.
-                    *  For each feature we sum the max values and min values from all the emotions weighed by their importance. */
-                    for(var j = 0; j < 4; j++){
-                        maxValSum += $scope.emotions[j].features[i].maxvalue * emotion_importance[$scope.emotions[j].emotion];
-                        minValSum += $scope.emotions[j].features[i].minvalue * emotion_importance[$scope.emotions[j].emotion];
-                    }
-                    var maxVal = maxValSum/4;
-                    var minVal = minValSum/4;
-                    feature_list.push({"feature":{"id":$scope.emotions[0].features[i].feature}, "minvalue":minVal,"maxvalue":maxVal});
+                /* Interpolate the features based on the distance to each emotion
+                   See http://www.speech.kth.se/prod/publications/files/1344.pdf for the algorithm used */
+                for(var i = 0; i < $scope.emotions[0].features.length;i++){
+                    var tr_max = $scope.topright.features[i].maxvalue;
+                    var tl_max = $scope.topleft.features[i].maxvalue;
+                    var bl_max = $scope.bottomleft.features[i].maxvalue;
+                    var br_max = $scope.bottomright.features[i].maxvalue;
+
+                    var tr_min = $scope.topright.features[i].minvalue;
+                    var tl_min = $scope.topleft.features[i].minvalue;
+                    var bl_min = $scope.bottomleft.features[i].minvalue;
+                    var br_min = $scope.bottomright.features[i].minvalue;
+
+                    var max_val = $scope.feature_variance+Math.round((0.25*y*(x*(tr_max - tl_max - br_max + bl_max) + tr_max + tl_max - br_max - bl_max))
+                            + (0.25*((x*(tr_max - tl_max + br_max - bl_max)) + tr_max + tl_max + br_max + bl_max)));
+
+                    var min_val = -$scope.feature_variance+Math.round(0.25*y*(x*(tr_min - tl_min - br_min + bl_min)
+                            + tr_min + tl_min - br_min - bl_min) + 0.25*(x*(tr_min - tl_min + br_min - bl_min) + tr_min + tl_min + br_min + bl_min));
+
+                    feature_list.push({"feature":{"id":$scope.emotions[0].features[i].feature}, "minvalue": min_val,"maxvalue": max_val});
                 }
                 return feature_list;
             };
@@ -112,25 +85,23 @@ angular.module('myApp.emotions', ['ngRoute'])
                 // Save the current variables used for the click
                 EmotionService.saveClick($scope.imgleft,$scope.imgtop,$scope.imgwidth,$scope.imgheight);
 
-                /* Calculate the importance and features required to request for music */
-                var emotion_importance = calcImportance(event.offsetX,event.offsetY,plane_width,plane_height);
-                var feature_list = calcFeatures(emotion_importance);
+                /* Calculate the features required to request for music */
+                var feature_list = calcFeatures(event.offsetX,event.offsetY,plane_width,plane_height);
                 SongRequestService.playMatchingSongs(feature_list);
             };
 
 
             /* Controller body starts here */
 
+            $scope.feature_variance = 5;
             //Get labels, initialize select boxes and load existing values
             Api.Labels.emotions().$promise.then(function(data){
                 $scope.emotions = data;
                 loadValues(data);
-                $scope.topleft = data[0].emotion;
-                $scope.bottomright = data[1].emotion;
-
-                $scope.topright = data[2].emotion;
-                $scope.bottomleft = data[3].emotion;
-                $scope.emotionnames = [$scope.topleft, $scope.topright, $scope.bottomleft, $scope.bottomright];
+                $scope.topleft = data[0];
+                $scope.bottomright = data[1];
+                $scope.topright = data[2];
+                $scope.bottomleft = data[3];
             }, function(err){
                 throw "No emotions were returned by query: "+err;
             });
