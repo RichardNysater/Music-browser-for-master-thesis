99f1b0367ae7a4136dc5db6e8e11506af878202e
RichardNysater <richard@nysater.org>
2015-12-11 19:57:31 +0100

    Added database template.

---
+++ create_song_table.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS Music;
+CREATE TABLE IF NOT EXISTS Music.songs
+(
+songID varchar(20),
+Dissonance int,
+Modality int,
+Speed int,
+RhythmicComplexity int,
+Articulation int,
+RhythmicClarity int,
+Pitch int,
+HarmonicComplexity int,
+Valence int,
+Activity int
+);
