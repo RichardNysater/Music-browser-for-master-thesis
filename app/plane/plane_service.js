68d11ce0c2b311fbf67f9eefd6149dbb8e51f9fb
RichardNysater <richard@nysater.org>
2015-12-28 17:36:39 +0100

    Now saves the location of the selection in the x-axis to help with window resizing. Added option to only save variables related to image size.

--- app/plane/plane_service.js
+++ app/plane/plane_service.js
@@ -15,8 +15,9 @@ PlaneService.factory('PlaneService', function () {
    * @param top The css-value for where the image should be displayed
    * @param width The width of the image
    * @param height The height of the image
+   * @param selection_percent The x-position of the selection marker in percent
    */
-  planeService.saveClick = function (variance, firstSelect, secondSelect, left, top, width, height) {
+  planeService.saveClick = function (variance, firstSelect, secondSelect, left, top, width, height, selection_percent) {
     planeService.variance = variance;
     planeService.firstSelect = firstSelect;
     planeService.secondSelect = secondSelect;
@@ -24,6 +25,17 @@ PlaneService.factory('PlaneService', function () {
     planeService.imgtop = top;
     planeService.imgwidth = width;
     planeService.imgheight = height;
+    planeService.selection_percent = selection_percent;
+  };
+
+  /**
+   * Should be called whenever window size is changed.
+   * @param left The css-value for where the image should be displayed
+   * @param selection_percent The x-position of the selection marker in percent
+   */
+  planeService.saveUpdatedWindow = function(left, selection_percent){
+    planeService.imgleft = left;
+    planeService.selection_percent = selection_percent;
   };
 
   return planeService;
