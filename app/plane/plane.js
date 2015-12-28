5c5bc03c3c99abbdad355a503764b3ad5e94f8e3
RichardNysater <richard@nysater.org>
2015-12-28 17:41:23 +0100

    Now correctly resizes the selection image when variance is changed and when the user resizes the window.

--- app/plane/plane.js
+++ app/plane/plane.js
@@ -9,8 +9,56 @@ angular.module('myApp.plane', ['ngRoute'])
     });
   }])
 
-  .controller('PlaneCtrl', ['$scope', 'Api', 'SongRequestService', 'PlaneService',
-    function ($scope, Api, SongRequestService, PlaneService) {
+  .controller('PlaneCtrl', ['$scope', 'Api', 'SongRequestService', 'PlaneService','$timeout',
+    function ($scope, Api, SongRequestService, PlaneService,$timeout) {
+
+      /**
+       * Set the size of the selection image
+       */
+      var setImageSize = function(){
+        var CSS_plane = $('.plane');
+        $scope.imgwidth = CSS_plane.outerWidth() * 1/(100/($scope.feature_variance*2));
+        $scope.imgheight = CSS_plane.outerHeight() * 1/(100/($scope.feature_variance*2));
+      };
+
+      /**
+       * Updates the selection image and plays songs based on the current selection after variance is changed
+       */
+      $scope.varianceChanged = function(){
+        var prevPageX = $scope.imgleft + ($scope.imgwidth/2);
+        var prevPageY = $scope.imgtop + ($scope.imgheight/2);
+        setImageSize();
+
+        $scope.imgleft = prevPageX - ($scope.imgwidth / 2);
+        $scope.imgtop = prevPageY - ($scope.imgheight / 2);
+        PlaneService.saveClick($scope.feature_variance, $scope.firstSelect, $scope.secondSelect, $scope.imgleft, $scope.imgtop, $scope.imgwidth, $scope.imgheight, getSelectionPercent());
+        playSongs();
+      };
+
+      /**
+       * Fetches and plays songs based on where the user clicked and what the variance is set to
+       */
+      var playSongs = function(){
+        // Create the min and max values for the two features based on the variance and where the user clicked
+        $scope.x_min_value = Math.max($scope.xpercent - $scope.feature_variance,0);
+        $scope.x_max_value = Math.min($scope.xpercent + $scope.feature_variance,100);
+
+        $scope.y_min_value = Math.max($scope.ypercent - $scope.feature_variance,0);
+        $scope.y_max_value = Math.min($scope.ypercent + $scope.feature_variance,100);
+
+        var xFeature = {
+          feature: {id: $scope.firstSelect.id},
+          minvalue: $scope.x_min_value,
+          maxvalue: $scope.x_max_value
+        };
+        var yFeature = {
+          feature: {id: $scope.secondSelect.id},
+          minvalue: $scope.y_min_value,
+          maxvalue: $scope.y_max_value
+        };
+
+        SongRequestService.playMatchingSongs([xFeature, yFeature]);
+      };
 
       /**
        * planeClick is called whenever a user clicks on the 2d-plane
@@ -18,13 +66,13 @@ angular.module('myApp.plane', ['ngRoute'])
        */
       $scope.planeClick = function (event) {
         var CSS_plane = $('.plane');
-        $scope.imgwidth = CSS_plane.outerWidth() * ($scope.feature_variance / 100);
-        $scope.imgheight = CSS_plane.outerHeight() * ($scope.feature_variance / 100);
+        setImageSize();
         $scope.imgleft = event.pageX - ($scope.imgwidth / 2);
         $scope.imgtop = event.pageY - ($scope.imgheight / 2);
 
         // Save the current variables used for the click
-        PlaneService.saveClick($scope.feature_variance, $scope.firstSelect, $scope.secondSelect, $scope.imgleft, $scope.imgtop, $scope.imgwidth, $scope.imgheight);
+        PlaneService.saveClick($scope.feature_variance, $scope.firstSelect, $scope.secondSelect, $scope.imgleft, $scope.imgtop, $scope.imgwidth, $scope.imgheight, getSelectionPercent());
+
 
         $scope.xc = event.offsetX;
         $scope.yc = event.offsetY;
@@ -32,19 +80,7 @@ angular.module('myApp.plane', ['ngRoute'])
         $scope.xpercent = Math.round(100 * ($scope.xc / CSS_plane.outerWidth()));
         $scope.ypercent = 100 - Math.round(100 * ($scope.yc / (CSS_plane.outerHeight())));
 
-        // Create the min and max values for the two features based on the variance and where the user clicked
-        var xFeature = {
-          feature: {id: $scope.firstSelect.id},
-          minvalue: $scope.xpercent - $scope.feature_variance,
-          maxvalue: $scope.xpercent + $scope.feature_variance
-        };
-        var yFeature = {
-          feature: {id: $scope.secondSelect.id},
-          minvalue: $scope.ypercent - $scope.feature_variance,
-          maxvalue: $scope.ypercent + $scope.feature_variance
-        };
-        SongRequestService.playMatchingSongs([xFeature, yFeature]);
-
+        playSongs();
       };
 
       /**
@@ -63,6 +99,10 @@ angular.module('myApp.plane', ['ngRoute'])
         $scope.imgleft = PlaneService.imgleft;
         $scope.imgtop = PlaneService.imgtop;
 
+        if(getSelectionPercent() != EmotionsService.selection_percent){
+          updateWindow();
+        }
+
         for (var i = 0; i < features.length; i++) { // Existing features should be selected in the select boxes
           if (PlaneService.firstSelect && features[i].id == PlaneService.firstSelect.id) {
             $scope.firstSelect = features[i];
@@ -72,6 +112,56 @@ angular.module('myApp.plane', ['ngRoute'])
             $scope.secondSelect = features[i];
           }
         }
+      };
+
+      /**
+       * Returns the inner width of the plane
+       * @returns {*|jQuery}
+       */
+      var getPlaneWidth = function(){
+        return $('.plane').innerWidth();
+      }
+
+      /**
+       * Return where on the plane's x-axis (width) the selection marker is
+       * @returns {number} 0 for the furthest left and 1 for furthest right
+       */
+      var getSelectionPercent = function(){
+        return ($scope.imgleft-$('.plane').offset().left)/getPlaneWidth();
+      }
+
+      /**
+       * This function is called whenever the window is resized
+       */
+      var updateWindow = function (){
+        setImageSize();
+        $scope.imgleft = $('.plane').offset().left + getPlaneWidth()*PlaneService.selection_percent;
+        PlaneService.saveUpdatedWindow($scope.imgleft, getSelectionPercent());
+      };
+
+      /**
+       * Initializes the handling of window resizing
+       */
+      var initWindowHandling = function(){
+        var oldWidth = getPlaneWidth();
+
+        $(window).on('resize.doResize', function () {
+          var newWidth = getPlaneWidth();
+          var updateStuffTimer;
+
+          if (newWidth !== oldWidth) {
+            $timeout.cancel(updateStuffTimer);
+          }
+
+          updateStuffTimer = $timeout(function() {
+            updateWindow();
+            oldWidth = newWidth;
+          }, 500);
+        });
+
+        $scope.$on('$destroy',function (){
+          $(window).off('resize.doResize'); // remove the handler added earlier
+        });
       };
 
 
@@ -92,5 +182,6 @@ angular.module('myApp.plane', ['ngRoute'])
         throw "No features were returned by query: " + err;
       });
 
-
+      // Handle window resizing
+      initWindowHandling();
     }]);
