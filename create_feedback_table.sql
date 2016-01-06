9fdb4f5b6f6447f92684a6be7f4cc963340a0508
RichardNysater <richard@nysater.org>
2016-01-06 16:38:03 +0100

    Added a database table to contain feedback from users.

---
+++ create_feedback_table.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS Music;
+CREATE TABLE IF NOT EXISTS Music.feedback
+(
+userID varchar(128),
+timestamp timestamp,
+questionID varchar(64),
+rating int,
+comment text,
+CONSTRAINT pk_userquestion PRIMARY KEY (userID,questionID)
+);
