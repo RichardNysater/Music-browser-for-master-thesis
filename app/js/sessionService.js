522f64f55db8a82dd06fd5a090afe1c96aa5bdf6
RichardNysater <richard@nysater.org>
2016-01-06 16:46:57 +0100

    Created a service to handle user sessions (currently tracks users using a userID in cookies)

---
+++ app/js/sessionService.js
@@ -0,0 +1,35 @@
+/**
+ * The session service handles the user session
+ */
+
+var SessionService = angular.module('SessionService', ['ngCookies']);
+
+SessionService.service('SessionService', ['$cookies',
+  function ($cookies) {
+
+    /**
+     * Generate a user ID for a new user
+     * @returns {string} A 36 byte user ID for a new user
+     */
+    var generateUserID = function () {
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+    };
+
+    /**
+     * Returns the user's userID or generates a new one and stores it if it does not yet exist
+     * @returns {*|string} The user's userID
+     */
+    this.getUserID = function () {
+      if (!$cookies.get('userID')) {
+        $cookies.put('userID', generateUserID());
+      }
+      return $cookies.get('userID');
+    };
+
+  }]);
