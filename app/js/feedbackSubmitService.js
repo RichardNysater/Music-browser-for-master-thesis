dacec0f9f9826f09c38a5bc23d2b31bcd4756622
RichardNysater <richard@nysater.org>
2016-01-06 16:42:37 +0100

    Created new service to submit user feedback.

---
+++ app/js/feedbackSubmitService.js
@@ -0,0 +1,27 @@
+var FeedbackSubmitService = angular.module('FeedbackSubmitService', ['ngResource','ngCookies']);
+
+/**
+ * Handle the sending of requests for songs to the server
+ */
+FeedbackSubmitService.service('FeedbackSubmitService', ['$resource', '$http', 'SessionService',
+  function ($resource, $http, SessionService) {
+
+    /**
+     * Submits the feedback to the server
+     * @param feedback The feedback to submit
+     * @returns {*} The request's promise
+     */
+    this.submitFeedback = function (feedback) {
+      feedback.userID = SessionService.getUserID();
+      var req = {
+        method: 'POST',
+        url: '/app/api/feedbackSubmit',
+        data: {
+          feedback: feedback
+        }
+      };
+      return $http(req);
+    };
+  }
+]);
+
