4fd15b890ffb02acdd31c3f682eb4e85b62b5880
RichardNysater <richard@nysater.org>
2016-01-09 17:38:12 +0100

    Added support for selectlist-type questions.

--- app/feedback/feedback.js
+++ app/feedback/feedback.js
@@ -16,7 +16,6 @@ angular.module('myApp.feedback', ['ngRoute'])
 
       /**
        * Whenever a user answers a question by clicking a button with a rating
-       * send the rating to the server to be added to the database
        * @param question The question being answered
        * @param rating The rating chosen
        */
@@ -27,17 +26,35 @@ angular.module('myApp.feedback', ['ngRoute'])
         FeedbackSubmitService.submitFeedback(feedback);
       };
 
+      /**
+       * Whenever the user answers a question by selecting an item in a selectlist
+       * @param question The question being answered
+       * @param option The option selected
+       */
+      $scope.selectedOption = function (question, option) {
+        question.selected = option;
+        var feedback = {questionID: question.id, comment: option.text};
+        FeedbackService.saveFeedback(question.id, option);
+        FeedbackSubmitService.submitFeedback(feedback);
+      };
+
       // Initialize values of buttons
       $scope.buttons = [{val: 1}, {val: 2}, {val: 3}, {val: 4}, {val: 5}, {val: 6}, {val: 7}, {val: 8}, {val: 9}];
 
       // Fetch and initialize the evaluation sections
       ResourcesService.Feedback.query().$promise.then(function (data) {
         // Load previously selected ratings for the questions
+        var prevFeedback = FeedbackService.getFeedback();
         for (var i = 0; i < data.length; i++) {
           for (var j = 0; j < data[i].questions.length; j++) {
-            var cur = FeedbackService.getFeedback()[data[i].questions[j].id]; //Exists if user previously answered question
-            if (cur) {
-              data[i].questions[j].selected = cur.rating;
+
+            var answer = prevFeedback[data[i].questions[j].id]; //Exists if user previously answered question
+            if (answer) {
+              data[i].questions[j].selected = answer;
+            }
+            else if (data[i].questions[j].type === "selectlist") {
+              data[i].questions[j].selected = data[i].questions[j].options[0]; //Pre-select first option
+              FeedbackSubmitService.submitFeedback({questionID: data[i].questions[j].id, comment:data[i].questions[j].selected.text});
             }
           }
         }
