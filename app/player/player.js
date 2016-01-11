dac47305fc4693deaf28bae45bdf097d8dc6aa37
RichardNysater <richard@nysater.org>
2016-01-11 01:08:44 +0100

    Added functions for volume slider and autoplay button.

--- app/player/player.js
+++ app/player/player.js
@@ -12,7 +12,28 @@ angular.module('myApp.player', ['ngRoute'])
   /**
    * The PlayerController handles the music player at the bottom of the website.
    */
-  .controller('PlayerController', ['$scope', 'ResourcesService', 'angularPlayer',
-    function ($scope, ResourcesService, angularPlayer) {
+  .controller('PlayerController', ['$scope','SongRequestService',
+    function ($scope,SongRequestService) {
+
+      /**
+       * Toggles whether songs should be autoplayed or not
+       */
+      $scope.toggleAutoPlay = function(){
+        SongRequestService.setAutoPlay(!SongRequestService.getAutoPlay());
+      };
+
+      /**
+       * Sets the volume of songs
+       * @param volume The volume to set songs to (0-100)
+       */
+      $scope.setVolume = function(volume){
+        SongRequestService.setVolume(volume);
+      };
+
+      var volumeSlider = $("#volumeSlider");
+      volumeSlider.slider();
+      volumeSlider.on("slide", function(slideEvt) {
+        $scope.setVolume(slideEvt.value);
+      });
 
     }]);
